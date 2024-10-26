9be317783066e19a636970417941afab96169849
rose <tamtammacro@gmail.com>
2024-10-26 03:37:01 -0700

    varref mul fix

--- src/asm_gen.lua
+++ src/asm_gen.lua
@@ -54,9 +54,9 @@ function AsmGen:make_variables()
         local value_attr = find_attr(decl_data.arguments,"value")
         local value = nil
 
-        if value_attr and value_attr.type_id == Nodes.BINOP then
+        if value_attr and value_attr.value.type_id == Nodes.NODE_BINOP then
             value = sum(value_attr.value)
-        elseif value_attr then
+        elseif value_attr and value_attr.value.type_id ~= Nodes.NODE_BINOP then
             value = value_attr.value
         else
             value = decl_data.value
@@ -86,40 +86,53 @@ function AsmGen:make_variables()
     end
 end
 
+function AsmGen:load_2_value_reg(data)
+    self.asm = self.asm..string.format("\tmov %s,%s\n",data.reg0 or "ebx",data.arg0)
+    self.asm = self.asm..string.format("\tmov %s,%s\n",data.reg1 or "ecx",data.arg1)
+end
+
 function AsmGen:read_ast()
     while self.current_node do
-        if self.current_node.type_id == Nodes.NODE_BINOP then
-            if self.current_node.left_node.token and self.current_node.right_node.token then
-                
-                -- mul check
-                if self.current_node.op_token.type_id == Token.MUL or self.current_node.op_token.type_id == Token.DIV then
-                    self.asm = self.asm..string.format("\tmov eax,%s\n",self.current_node.left_node.token.value)
-                    self.asm = self.asm..string.format("\tmov ebx,%s\n",self.current_node.right_node.token.value)
-                else
-                    self.asm = self.asm..string.format("\tmov ebx,%s\n",self.current_node.left_node.token.value)
-                    self.asm = self.asm..string.format("\tmov ecx,%s\n",self.current_node.right_node.token.value)
-                end
-                -- mul check end
+        local node = self.current_node
 
-            elseif self.current_node.left_node.type_id == Nodes.VAR_REF then
-                self.asm = self.asm..string.format("\tmov ebx,[%s]\n",self.current_node.left_node.name)
-                self.asm = self.asm..string.format("\tmov ecx,[%s]\n",self.current_node.right_node.name)
+        if node.type_id == Nodes.NODE_BINOP then
+            if node.left_node.token and node.right_node.token then
+            local data = {arg0 = node.left_node.token.value,arg1 = node.right_node.token.value}
+
+            if node.op_token.type_id == Token.MUL or node.op_token.type_id == Token.DIV then
+                data.reg0 = "eax"
+                data.reg1 = "ebx"
+                self:load_2_value_reg(data)
+            else
+                self:load_2_value_reg(data)
             end
-            if (self.current_node.op_token.type_id == Token.PLUS) then
-                self.asm = self.asm.."\tadd ebx,ecx\n\n"
+                
+            elseif node.left_node.type_id == Nodes.VAR_REF then
+                local data = {arg0 = string.format("[%s]",node.left_node.name),arg1 = string.format("[%s]",node.right_node.name)}
+
+                if node.op_token.type_id == Token.MUL or node.op_token.type_id == Token.DIV then
+                    data.reg0 = "eax"
+                    data.reg1 = "ebx"
+                    self:load_2_value_reg(data)
+                else
+                    self:load_2_value_reg(data)
+                end
+            end
+            if (node.op_token.type_id == Token.PLUS) then
+                self.asm = self.asm.."\tadd ebx,ecx\n"
             elseif (self.current_node.op_token.type_id == Token.SUB) then
-                self.asm = self.asm.."\tsub ebx,ecx\n\n"
-            elseif (self.current_node.op_token.type_id == Token.MUL) then
-                self.asm = self.asm.."\tmul ebx\n\n"
+                self.asm = self.asm.."\tsub ebx,ecx\n"
+            elseif (node.op_token.type_id == Token.MUL) then
+                self.asm = self.asm.."\tmul ebx\n"
                 self.asm = self.asm.."\tmov ebx,eax\n\n"
-            elseif (self.current_node.op_token.type_id == Token.DIV) then
-                self.asm = self.asm.."\tdiv ebx\n\n"
-                self.asm = self.asm.."\tmov ebx,eax\n\n"
+            elseif (node.op_token.type_id == Token.DIV) then
+                self.asm = self.asm.."\tdiv ebx\n"
+                self.asm = self.asm.."\tmov ebx,eax\n"
             end
-        elseif self.current_node.type_id == Nodes.NODE_NUMBER then
-            self.asm = self.asm..string.format("\tmov ebx,%s\n",self.current_node.token.value)
+        elseif node.type_id == Nodes.NODE_NUMBER then
+            self.asm = self.asm..string.format("\tmov ebx,%s\n",node.token.value)
         elseif self.current_node.type_id == Nodes.VAR_REF then
-            self.asm = self.asm..string.format("\tmov ebx,[%s]\n",self.current_node.name)
+            self.asm = self.asm..string.format("\tmov ebx,[%s]\n",node.name)
         end
         self:advance()
     end
